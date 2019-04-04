97784bb9b6b760040a797c0f872eaad9a4b9312a
Takahiro Yano <speg03@gmail.com>
2019-04-04 22:17:09 +0900

    :sparkles: Create topic for notifications

--- aws/lambda/email/hello_world/app.py
+++ aws/lambda/email/hello_world/app.py
@@ -1,8 +1,5 @@
-import json
 import os
 
 
 def lambda_handler(event, context):
-    env = dict(os.environ)
-    print(json.dumps(env, indent=2))
-    return 'OK'
+    return os.getenv('SNS_TOPIC_ARN', 'N/A')
