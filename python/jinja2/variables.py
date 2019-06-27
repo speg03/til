58dd2d45188cefdfa8e117c31f3b8bc2bacb7f97
Takahiro Yano <speg03@gmail.com>
2019-06-27 23:26:36 +0900

    :art: Add logging

--- python/jinja2/variables.py
+++ python/jinja2/variables.py
@@ -6,13 +6,14 @@ template_str = 'hello, {{ message }}'
 
 env = Environment()
 ast = env.parse(template_str)
-variables = set(meta.find_undeclared_variables(ast))
+variables = meta.find_undeclared_variables(ast)
+print('variables:', variables)
 
 parser = argparse.ArgumentParser()
 for v in variables:
     parser.add_argument('--' + v, required=True)
-
 args = parser.parse_args()
+print('args:', args)
 
 template = env.from_string(template_str)
 print(template.render(**vars(args)))
