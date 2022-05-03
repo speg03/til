8471b7bfa4010622a382824cf9a664b013391683
Takahiro Yano <speg03@gmail.com>
2022-05-03 09:25:50 +0900

    :rocket: 20220503-python-aiplatform-submit (#34)
    * :rocket: 20220503-python-aiplatform-submit
    
    * Add a submitting script for Vertex AI Pipelines

---
+++ python/kfp/cli/v2/submit.py
@@ -0,0 +1,21 @@
+import argparse
+import os
+
+from google.cloud import aiplatform
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("package_path")
+    args = parser.parse_args()
+
+    aiplatform.PipelineJob(
+        display_name=os.path.basename(args.package_path),
+        template_path=args.package_path,
+        pipeline_root="gs://speg03-bucket/pipeline_root/",
+        parameter_values={"msg": "hello, Vertex AI Pipelines"},
+    ).submit()
+
+
+if __name__ == "__main__":
+    main()
